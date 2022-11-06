78f88f32c792beedb9cc912213c007dc51c4c133
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-06 15:47:18 -0300

    Update InicioFragment.kt

--- app/src/main/java/com/example/orangefintech/ui/fragmentos/InicioFragment.kt
+++ app/src/main/java/com/example/orangefintech/ui/fragmentos/InicioFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.orangefintech.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,12 +23,25 @@ class inicioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var irACompra: Button
+    private lateinit var irAPerfil: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        irACompra = requireView().findViewById<Button>(R.id.compraFragment)
+        irAPerfil = requireView().findViewById<Button>(R.id.perfilFragment)
+
+        irACompra.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment2_to_compraFragment2)
+        }
+        
+        irAPerfil.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment2_to_perfilFragment2)
         }
     }
 
