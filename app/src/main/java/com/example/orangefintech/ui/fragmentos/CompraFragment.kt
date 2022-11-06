4b403093694731b0ca18a2233840451c807c67b6
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-06 16:03:16 -0300

    Update CompraFragment.kt

--- app/src/main/java/com/example/orangefintech/ui/fragmentos/CompraFragment.kt
+++ app/src/main/java/com/example/orangefintech/ui/fragmentos/CompraFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.orangefintech.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,11 +24,22 @@ class CompraFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var irAPerfil: Button
+    private lateinit var irAInicio: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        irAPerfil.setOnClickListener {
+            findNavController().navigate(R.id.action_compraFragment2_to_perfilFragment22)
+        }
+
+        irAInicio.setOnClickListener {
+            findNavController().navigate(R.id.action_compraFragment2_to_inicioFragment2)
         }
     }
 
