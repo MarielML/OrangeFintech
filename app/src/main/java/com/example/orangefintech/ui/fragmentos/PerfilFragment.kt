0985111d6199f1f7760f7c3501b1c172fc544019
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-06 17:43:37 -0300

    Update PerfilFragment.kt

--- app/src/main/java/com/example/orangefintech/ui/fragmentos/PerfilFragment.kt
+++ app/src/main/java/com/example/orangefintech/ui/fragmentos/PerfilFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.orangefintech.R
+import com.google.android.material.navigation.NavigationBarView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,8 +23,6 @@ class PerfilFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var irACompra: Button
-    private lateinit var irAInicio: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,15 +31,18 @@ class PerfilFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
 
-        irACompra = requireView().findViewById<Button>(R.id.compraFragment)
-        irAInicio = requireView().findViewById<Button>(R.id.inicioFragment)
-
-        irACompra.setOnClickListener {
-            findNavController().navigate(R.id.action_perfilFragment2_to_compraFragment2)
-        }
-
-        irAInicio.setOnClickListener {
-            findNavController().navigate(R.id.action_perfilFragment2_to_inicioFragment2)
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.compraFragment -> {
+                    findNavController().navigate(R.id.action_perfilFragment2_to_compraFragment2)
+                    true
+                }
+                R.id.inicioFragment -> {
+                    findNavController().navigate(R.id.action_perfilFragment2_to_inicioFragment2)
+                    true
+                }
+                else -> false
+            }
         }
     }
 
