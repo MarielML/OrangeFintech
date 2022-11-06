56b88a2e06b35f0db4642ba472adfdf23b471bb1
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-06 13:55:26 -0300

    Update PantallaPrincipalActivity.kt

--- app/src/main/java/com/example/orangefintech/ui/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/orangefintech/ui/PantallaPrincipalActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.orangefintech.R
@@ -14,6 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class PantallaPrincipalActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
 
     private lateinit var spinner: Spinner
+    private lateinit var inicio: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,11 @@ class PantallaPrincipalActivity : AppCompatActivity(), AdapterView.OnItemSelecte
         val navController = findNavController(R.id.fragmentContainerViewPantallaPrincipal)
         bottomNavigationView.setupWithNavController(navController)
 
+        inicio = findViewById(R.id.inicioFragment2)
+        inicio.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_compraFragment2_to_perfilFragment22)
+        }
+
         spinner = findViewById(R.id.spinnerExchanges)
         ArrayAdapter.createFromResource(this, R.array.exchange_array, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -31,14 +39,11 @@ class PantallaPrincipalActivity : AppCompatActivity(), AdapterView.OnItemSelecte
         spinner.onItemSelectedListener = this
     }
 
+
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
         TODO("Not yet implemented")
-        var exchange = spinner.selectedItem.toString()
-        when(exchange){
-            "Criptomas" -> ""
-            "Criptodia" -> ""
-            "Carrecripto" -> ""
-        }
+
+
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
