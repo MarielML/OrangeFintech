76dadfff04e3af7599074f7076b11c2dd7c6691f
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-06 17:43:10 -0300

    Update InicioFragment.kt

--- app/src/main/java/com/example/orangefintech/ui/fragmentos/InicioFragment.kt
+++ app/src/main/java/com/example/orangefintech/ui/fragmentos/InicioFragment.kt
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PerfilFragment.newInstance] factory method to
+ * Use the [inicioFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PerfilFragment : Fragment() {
+class inicioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,11 +34,11 @@ class PerfilFragment : Fragment() {
         NavigationBarView.OnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.compraFragment -> {
-                    findNavController().navigate(R.id.action_perfilFragment2_to_compraFragment2)
+                    findNavController().navigate(R.id.action_inicioFragment2_to_compraFragment2)
                     true
                 }
-                R.id.inicioFragment -> {
-                    findNavController().navigate(R.id.action_perfilFragment2_to_inicioFragment2)
+                R.id.perfilFragment -> {
+                    findNavController().navigate(R.id.action_inicioFragment2_to_perfilFragment2)
                     true
                 }
                 else -> false
@@ -51,7 +51,7 @@ class PerfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        return inflater.inflate(R.layout.fragment_inicio, container, false)
     }
 
     companion object {
@@ -61,12 +61,12 @@ class PerfilFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PerfilFragment.
+         * @return A new instance of fragment inicioFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PerfilFragment().apply {
+            inicioFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
