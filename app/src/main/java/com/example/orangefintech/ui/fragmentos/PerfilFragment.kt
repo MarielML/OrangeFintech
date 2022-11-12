84f8416f1d8e50a2241c999858d6a1ebcc8eb947
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-12 17:51:57 -0300

    Update PerfilFragment.kt

--- app/src/main/java/com/example/orangefintech/ui/fragmentos/PerfilFragment.kt
+++ app/src/main/java/com/example/orangefintech/ui/fragmentos/PerfilFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.orangefintech.R
 import com.google.android.material.navigation.NavigationBarView
@@ -23,6 +24,8 @@ class PerfilFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var cuenta: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
