ca4438412de2f3d9173d6cd0434e1dfd428be6ea
Mariel <111444526+MarielML@users.noreply.github.com>
2022-11-23 16:24:02 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/orangefintech/adapters/ComprasAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.orangefintech.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orangefintech.databinding.ItemCompraBinding
+import com.example.orangefintech.entidades.Compra
+
+class ComprasAdapter (private val compras: List<Compra>, private val selectClientClickLister: (Compra) -> Unit) : RecyclerView.Adapter<ComprasAdapter.ComprasViewHolder>(){
+
+    class ComprasViewHolder (val binding: ItemCompraBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComprasViewHolder {
+        val compraBinding = ItemCompraBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ComprasViewHolder(compraBinding)
+    }
+
+    override fun onBindViewHolder(holder: ComprasViewHolder, position: Int) {
+        val compra = compras[position]
+
+        holder.binding.txtCodigo.text = compra.codigoCompra.toString()
+        holder.binding.txtFecha.text = compra.fechaCompra.toString()
+        holder.binding.txtHora.text = compra.horaCompra.toString()
+
+        holder.binding.comprasCard.setOnClickListener {
+            selectClientClickLister(compra)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return compras.size
+    }
+}
